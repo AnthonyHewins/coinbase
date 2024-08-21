b982d29f1a8d36a327bd67bd1da14c466c3477ad
anthony <aahewins@protonmail.com>
2024-08-21 08:19:33 -0400

    version

--- order.go
+++ order.go
@@ -1,36 +1,139 @@
 package coinbase
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 )
 
+//go:generate enumer -type Side -json -trimprefix Side -transform upper
+type Side byte
+
+const (
+	SideUnspecified Side = iota
+	SideBuy
+	SideSell
+)
+
+type OrderType byte
+
+const (
+	OrderTypeUnspecified OrderType = iota
+	Market
+	LimitIOC // Immediate or cancel (has to fill some part of the order immediately, if it fails, cancel the rest)
+	LimitFOK // Fill or Kill
+	LimitGTC // Good til Canceled
+	LimitGTD // Good til date
+	StopLimitGTC
+	StopLimitGTD
+	TriggerBracketGTC
+	TriggerBracketGTD
+)
+
+type OrderConfig interface {
+	orderType() OrderType
+}
+
+//go:generate enumer -type MarginType -json -transform upper -trimprefix MarginType
+type MarginType byte
+
+const (
+	MarginTypeUnspecified MarginType = iota
+	MarginTypeCross
+	MarginTypeIsolated
+)
+
 type Order struct {
-	Type      string `json:"type"`
-	Size      string `json:"size,omitempty"`
-	Side      string `json:"side"`
-	ProductID string `json:"product_id"`
-	ClientOID string `json:"client_oid,omitempty"`
-	Stp       string `json:"stp,omitempty"`
-	Stop      string `json:"stop,omitempty"`
-	StopPrice string `json:"stop_price,omitempty"`
-	// Limit Order
-	Price       string `json:"price,omitempty"`
-	TimeInForce string `json:"time_in_force,omitempty"`
-	PostOnly    bool   `json:"post_only,omitempty"`
-	CancelAfter string `json:"cancel_after,omitempty"`
-	// Market Order
-	Funds          string `json:"funds,omitempty"`
-	SpecifiedFunds string `json:"specified_funds,omitempty"`
-	// Response Fields
-	ID            string `json:"id"`
-	Status        string `json:"status,omitempty"`
-	Settled       bool   `json:"settled,omitempty"`
-	DoneReason    string `json:"done_reason,omitempty"`
-	DoneAt        Time   `json:"done_at,string,omitempty"`
-	CreatedAt     Time   `json:"created_at,string,omitempty"`
-	FillFees      string `json:"fill_fees,omitempty"`
-	FilledSize    string `json:"filled_size,omitempty"`
-	ExecutedValue string `json:"executed_value,omitempty"`
+	ID        string      `json:"client_order_id"`
+	ProductID string      `json:"product_id"`
+	Side      Side        `json:"side"`
+	Config    OrderConfig `json:"order_configuration"`
+
+	// Optional: empty string will omit leverage
+	Leverage string `json:"leverage"`
+
+	// Optional: default value will not send
+	MarginType MarginType `json:"margin_type"`
+
+	// Optional: empty string will omit
+	RetailPortfolioID string `json:"retail_portfolio_id"`
+
+	// Optional: empty string will omit
+	PreviewID string `json:"preview_id"`
+	// Type      string `json:"type"`
+	// Size      string `json:"size,omitempty"`
+	// ProductID string `json:"product_id"`
+	// ClientOID string `json:"client_oid,omitempty"`
+	// Stp       string `json:"stp,omitempty"`
+	// Stop      string `json:"stop,omitempty"`
+	// StopPrice string `json:"stop_price,omitempty"`
+	// // Limit Order
+	// Price       string `json:"price,omitempty"`
+	// TimeInForce string `json:"time_in_force,omitempty"`
+	// PostOnly    bool   `json:"post_only,omitempty"`
+	// CancelAfter string `json:"cancel_after,omitempty"`
+	// // Market Order
+	// Funds          string `json:"funds,omitempty"`
+	// SpecifiedFunds string `json:"specified_funds,omitempty"`
+	// // Response Fields
+	// ID            string `json:"id"`
+	// Status        string `json:"status,omitempty"`
+	// Settled       bool   `json:"settled,omitempty"`
+	// DoneReason    string `json:"done_reason,omitempty"`
+	// DoneAt        Time   `json:"done_at,string,omitempty"`
+	// CreatedAt     Time   `json:"created_at,string,omitempty"`
+	// FillFees      string `json:"fill_fees,omitempty"`
+	// FilledSize    string `json:"filled_size,omitempty"`
+	// ExecutedValue string `json:"executed_value,omitempty"`
+}
+
+func (o *Order) MarshalJSON() ([]byte, error) {
+	b := bytes.NewBuffer([]byte(`{"client_order_id":`))
+	e := json.NewEncoder(b)
+	e.SetIndent("", "")
+
+	if err := e.Encode(o.ID); err != nil {
+		return nil, err
+	}
+	b.WriteString(`,"product_id":`)
+
+	if err := e.Encode(o.ProductID); err != nil {
+		return nil, err
+	}
+
+	if o.Config != nil {
+		b.WriteString(`,"order_configuration":`)
+		if err := e.Encode(o.Config); err != nil {
+			return nil, err
+		}
+	}
+
+	type strFields struct {
+		name, value string
+	}
+
+	for _, v := range []strFields{
+		{"leverage", o.Leverage},
+		{"retail_portfolio_id", o.RetailPortfolioID},
+		{"preview_id", o.PreviewID},
+	} {
+		if v.value != "" {
+			b.WriteString(fmt.Sprintf(`,"%s":`, v.name))
+			if err := e.Encode(v.value); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	if o.MarginType != MarginTypeUnspecified {
+		b.WriteString(`,"margin_type":`)
+		if err := e.Encode(o.MarginType); err != nil {
+			return nil, err
+		}
+	}
+
+	b.WriteRune('}')
+	return b.Bytes(), nil
 }
 
 type CancelAllOrdersParams struct {
@@ -45,10 +148,6 @@ type ListOrdersParams struct {
 
 func (c *Client) CreateOrder(newOrder *Order) (Order, error) {
 	var savedOrder Order
-
-	if len(newOrder.Type) == 0 {
-		newOrder.Type = "limit"
-	}
 
 	url := fmt.Sprintf("/orders")
 	_, err := c.Request("POST", url, newOrder, &savedOrder)
--- order_test.go
+++ order_test.go
@@ -1,167 +1,30 @@
 package coinbase
 
 import (
-	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateLimitOrders(t *testing.T) {
-	client := NewTestClient()
-
-	order := Order{
-		Price:     "1.00000000",
-		Size:      "1000.00000000",
-		Side:      "buy",
-		ProductID: "BTC-USD",
+func TestOrderMarshal(mainTest *testing.T) {
+	testCases := []struct {
+		name        string
+		arg         Order
+		expected    string
+		expectedErr error
+	}{
+		{
+			name:        "base case",
+			arg:         Order{},
+			expected:    "",
+			expectedErr: nil,
+		},
 	}
 
-	savedOrder, err := client.CreateOrder(&order)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if savedOrder.ID == "" {
-		t.Error(errors.New("No create id found"))
-	}
-
-	props := []string{"Price", "Size", "Side", "ProductID"}
-	_, err = CompareProperties(order, savedOrder, props)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := client.CancelOrder(savedOrder.ID); err != nil {
-		t.Error(err)
-	}
-}
-
-func TestCreateMarketOrders(t *testing.T) {
-	client := NewTestClient()
-
-	order := Order{
-		Funds:     "10.00",
-		Size:      "1000.00000000",
-		Side:      "buy",
-		Type:      "market",
-		ProductID: "BTC-USD",
-	}
-
-	savedOrder, err := client.CreateOrder(&order)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if savedOrder.ID == "" {
-		t.Error(errors.New("No create id found"))
-	}
-
-	props := []string{"Price", "Size", "Side", "ProductID"}
-	_, err = CompareProperties(order, savedOrder, props)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestCancelOrder(t *testing.T) {
-	client := NewTestClient()
-
-	order := Order{
-		Price:     "1.00",
-		Size:      "1000.00",
-		Side:      "buy",
-		ProductID: "BTC-USD",
-	}
-
-	savedOrder, err := client.CreateOrder(&order)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := client.CancelOrder(savedOrder.ID); err != nil {
-		t.Error(err)
-		t.Error(err)
-	}
-}
-
-func TestGetOrder(t *testing.T) {
-	client := NewTestClient()
-
-	order := Order{
-		Price:     "1.00",
-		Size:      "1.00",
-		Side:      "buy",
-		ProductID: "BTC-USD",
-	}
-
-	savedOrder, err := client.CreateOrder(&order)
-	if err != nil {
-		t.Error(err)
-	}
-
-	getOrder, err := client.GetOrder(savedOrder.ID)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if getOrder.ID != savedOrder.ID {
-		t.Error(errors.New("Order ids do not match"))
-	}
-
-	if err := client.CancelOrder(savedOrder.ID); err != nil {
-		t.Error(err)
-	}
-}
-
-func TestListOrders(t *testing.T) {
-	client := NewTestClient()
-	cursor := client.ListOrders()
-	var orders []Order
-
-	for cursor.HasMore {
-		if err := cursor.NextPage(&orders); err != nil {
-			t.Error(err)
-		}
-
-		for _, o := range orders {
-			if StructHasZeroValues(o) {
-				t.Error(errors.New("Zero value"))
-			}
-		}
-	}
-
-	cursor = client.ListOrders(ListOrdersParams{Status: "open", ProductID: "BTC-EUR"})
-	for cursor.HasMore {
-		if err := cursor.NextPage(&orders); err != nil {
-			t.Error(err)
-		}
-
-		for _, o := range orders {
-			if StructHasZeroValues(o) {
-				t.Error(errors.New("Zero value"))
-			}
-		}
-	}
-}
-
-func TestCancelAllOrders(t *testing.T) {
-	client := NewTestClient()
-
-	for _, pair := range []string{"BTC-USD"} {
-		for i := 0; i < 2; i++ {
-			order := Order{Price: "100000.00", Size: "1.00", Side: "sell", ProductID: pair}
-
-			if _, err := client.CreateOrder(&order); err != nil {
-				t.Error(err)
-			}
-		}
-
-		orderIDs, err := client.CancelAllOrders(CancelAllOrdersParams{ProductID: pair})
-		if err != nil {
-			t.Error(err)
-		}
-
-		if len(orderIDs) != 2 {
-			t.Error("Did not cancel all orders")
-		}
+	t := assert.New(mainTest)
+	for _, tc := range testCases {
+		actual, actualErr := tc.arg.MarshalJSON()
+		t.Equal(tc.expected, string(actual), tc.name)
+		t.Equal(tc.expectedErr, actualErr, tc.name)
 	}
 }
